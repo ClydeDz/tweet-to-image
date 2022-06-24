4e1e1dded846f145f03b744bef11dcc55d954717
clydedz <clyd94@gmail.com>
2022-06-24 23:05:59 +1200

    Move header into own component

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,9 @@ import {
   Switch,
   Textarea,
   TextInput,
-  Title,
-  Text,
-  Anchor,
-  Center
 } from "@mantine/core";
 import { At } from "tabler-icons-react";
+import AppHeader from "./components/AppHeader/AppHeader";
 
 const getTwitterAvatarUrl = (username: string): string => {
   return `https://unavatar.io/twitter/${username}?
@@ -112,26 +109,7 @@ function App() {
       </div>
       <Grid justify="center" grow gutter="xs" style={{marginRight: "0"}}>
         <Grid.Col className="header-container">
-          <Title order={1} align="center">Tweet to image</Title>
-          <Text align="center">Download a Tweet as an Instagram-ready image</Text>
-          <Center>
-            <Anchor href="https://github.com/ClydeDz/tweet-to-image/issues/new/choose"
-              target="_blank" align="center" className="anchor" size="sm">
-              Have an idea?
-            </Anchor>
-            <Anchor href="https://github.com/ClydeDz/tweet-to-image/issues/new/choose"
-              target="_blank" align="center" className="anchor" size="sm">
-              Report an issue
-            </Anchor>
-            <Anchor href="https://sponsor.clydedsouza.net/" target="_blank"
-              align="center" className="anchor" size="sm">
-              Support
-            </Anchor>
-            <Anchor href="https://clydedsouza.net/" target="_blank" 
-              align="center" className="anchor" size="sm">
-              Developed by Clyde D'Souza
-            </Anchor>
-          </Center>
+          <AppHeader />
         </Grid.Col>
       </Grid>
       <Grid justify="center" grow gutter="xs" style={{marginRight: "0"}}>
---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,30 @@
+import { Anchor, Center, Title, Text } from "@mantine/core";
+
+const AppHeader = () => {
+    return (
+        <>
+            <Title order={1} align="center">Tweet to image</Title>
+            <Text align="center">Download a Tweet as an Instagram-ready image</Text>
+            <Center>
+            <Anchor href="https://github.com/ClydeDz/tweet-to-image/issues/new/choose"
+                target="_blank" align="center" className="anchor" size="sm">
+                Have an idea?
+            </Anchor>
+            <Anchor href="https://github.com/ClydeDz/tweet-to-image/issues/new/choose"
+                target="_blank" align="center" className="anchor" size="sm">
+                Report an issue
+            </Anchor>
+            <Anchor href="https://sponsor.clydedsouza.net/" target="_blank"
+                align="center" className="anchor" size="sm">
+                Support
+            </Anchor>
+            <Anchor href="https://clydedsouza.net/" target="_blank" 
+                align="center" className="anchor" size="sm">
+                Developed by Clyde D'Souza
+            </Anchor>
+            </Center>
+        </>
+    );
+};
+
+export default AppHeader;
