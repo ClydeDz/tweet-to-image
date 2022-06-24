326c50fd7ff07b091a5ceb560bda3c920ce7bfdd
clydedz <clyd94@gmail.com>
2022-06-24 22:39:37 +1200

    Make font size smaller

--- src/App.tsx
+++ src/App.tsx
@@ -116,19 +116,19 @@ function App() {
           <Text align="center">Download a Tweet as an Instagram-ready image</Text>
           <Center>
             <Anchor href="https://github.com/ClydeDz/tweet-to-image/issues/new/choose"
-              target="_blank" align="center" className="anchor">
+              target="_blank" align="center" className="anchor" size="sm">
               Have an idea?
             </Anchor>
             <Anchor href="https://github.com/ClydeDz/tweet-to-image/issues/new/choose"
-              target="_blank" align="center" className="anchor">
+              target="_blank" align="center" className="anchor" size="sm">
               Report an issue
             </Anchor>
             <Anchor href="https://sponsor.clydedsouza.net/" target="_blank"
-              align="center" className="anchor">
+              align="center" className="anchor" size="sm">
               Support
             </Anchor>
             <Anchor href="https://clydedsouza.net/" target="_blank" 
-              align="center" className="anchor">
+              align="center" className="anchor" size="sm">
               Developed by Clyde D'Souza
             </Anchor>
           </Center>
