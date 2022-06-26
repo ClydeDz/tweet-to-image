1715848ec0813daf4c62c84311d290eb3866826c
clydedz <clyd94@gmail.com>
2022-06-26 22:57:27 +1200

    Revert test file

--- src/components/AppHeader/AppHeader.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import AppHeader from "./AppHeader";
-
-test("should render correctly", () => {
-    render(<AppHeader />);
-    expect(screen.getByText("Tweet to Image")).toBeInTheDocument();
-});
